0c208aa2b761d6b0495e5bec85143e4b4639790f
Adrian Nordin <adde--97@hotmail.com>
2018-04-22 16:22:01 +0200

    update

--- .vscode/Labration 2/streamClient.py
+++ .vscode/Labration 2/streamClient.py
@@ -3,23 +3,22 @@ from time import sleep
 
 host = ''
 port = 8080
-frequence = 0.1  #time in seconds
-packageCounter = 0
-packageAmount = 40
+frequence = 0.001  #time in seconds
+packageCounter = 1
+packageAmount = 100
 packageSize = 1000 #size in bytes
-sequenceNumber = 10000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 
 data = ""
-for i in range(0, 100):
+for i in range(0, packageSize / 10):
     data += "ABCDEFGHIJ"
 
 
 
 while packageCounter < packageAmount:
-    msg = str(sequenceNumber + packageCounter) + ";" + data
+    msg = str(packageCounter).zfill(5) + ";" + data
     packageCounter += 1
     print("Packet sent: " + msg[0:8] + "...\n")
     clientSocket.sendto(msg.encode(),(host, port))
--- .vscode/Labration 2/streamReceiver.py
+++ .vscode/Labration 2/streamReceiver.py
@@ -1,21 +1,29 @@
 from socket import *
 
-host = ''
+host = '192.168.0.4'
 port = 8080
 
 receiverSocket = socket(AF_INET, SOCK_DGRAM)
 receiverSocket.bind((host, port))
+lastRecieved = 1
 
 print ("Server socket bound to port: %d and host: %s" % (port, host))
 
 print ("The UDP server is ready to recieve")
+showErrors = True
 
-while True:
+while 1:
     # read client's message and remember client's address (IP and port)
     message, clientAddress = receiverSocket.recvfrom(2048)
 
+    sequenceNumber = int(message.decode()[0:5])
+    if (sequenceNumber != lastRecieved) and showErrors:
+        print("ERROR: expected packet:%d, got packet:%d\n" % (lastRecieved, sequenceNumber))
+    lastRecieved += 1
+
     # Print message and client address
-    print (message.decode()[0:20] + "...\n")
-    print (clientAddress)
+    if showErrors == False:
+        print (message.decode()[0:20] + "...\n")
+        print (clientAddress)
 
 
