d8b5f2603f80d8ce67255f64b1b74c67c26924d4
Adrian Nordin <adde--97@hotmail.com>
2018-04-23 17:49:51 +0200

    commit

--- .vscode/Labration 2/streamClient.py
+++ .vscode/Labration 2/streamClient.py
@@ -1,27 +1,29 @@
 from socket import *
 from time import sleep
 
-host = '192.168.0.4'
+host = '192.168.0.3'
 port = 8080
-frequence = 21  #meddelanden/s
-packageCounter = 1
-packageAmount = 200
-packageSize = 1000 #size in bytes
+frequence = 20  # medd/s
+packageCounter = 1 # for sequence number
+timer = 10 # how long will you send packets for
+packageSize = 1000 # size in bytes
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 
 data = ""
 
-for i in range(0, 100):
-    data += "ABCDEFGHIJ"
+for i in range(0, packageSize):
+    data += "A"
 
+frequence = 1/frequence
+dt = 0
 
-
-while packageCounter < packageAmount:
+while dt < timer:
     msg = str(packageCounter).zfill(5) + ";" + data
     packageCounter += 1
     print("Packet sent: " + msg[0:8] + "...\n")
     clientSocket.sendto(msg.encode(),(host, port))
-    sleep(1/frequence)
+    dt += frequence
+    sleep(frequence)
  
